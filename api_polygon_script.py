cc0f268276d24846cc08e05b77aeca2424cd3360
Adam Oh <adam@thegoodneighbor.io>
2023-07-30 15:59:34 -0400

    update v1

--- api_polygon_script.py
+++ api_polygon_script.py
@@ -1,12 +1,13 @@
 import json
 import requests
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # API key
 API_KEY = 'PGY2hHN4ls4x_sN3uk0FhIluBcMsLuJT'
 
 # Polygon API URL
-API_URL = f'https://api.polygon.io/v2/aggs/ticker/META/range/1/day/2023-01-09/2023-01-09?adjusted=true&sort=asc&apiKey={API_KEY}'
+API_URL = f'https://api.polygon.io/v2/aggs/ticker/META/range/1/day/2023-01-01/2023-07-20?adjusted=true&sort=asc&apiKey={API_KEY}'
 print('Printing API URL:...')
 print(API_URL)
 
@@ -22,7 +23,16 @@ json_data = json.loads(response.text)
 
 # Convert JSON into pandas dataframe
 df = pd.DataFrame(json_data['results'])
-print(df.head())
 
-# save the dataframe to csv
-df.to_csv('polygon_data.csv')
+# mean statistics
+print('Printing mean statistics...')
+print(df.describe())
+
+# visualize data using matplotlib
+plt.plot(df['c'], color='green')
+plt.plot(df['h'], color='black')
+plt.plot(df['l'], color='red')
+plt.title('Polygon.io Stock Price')
+plt.xlabel('Time')
+plt.ylabel('Price')
+plt.show()
